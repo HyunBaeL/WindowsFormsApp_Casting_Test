e73a77c27b2b5199a71a56aa9c1499abd6c48e01
HyunBaeL <seehb126@naver.com>
2025-05-13 11:15:04 +0900

    Add project files.

---
+++ WindowsFormsApp_Casting_Test/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_Casting_Test
+{
+    public class Animal
+    {
+        public string Name {  get; set; }
+
+        public void Speak()
+        {
+            Console.WriteLine($"{Name} 동물이 소리를 냅니다.");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        
+        public Dog() : base() { }
+
+        public void DogSound()
+        {
+            Console.WriteLine("강아지 울음소리 멍멍~");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat() : base() { }
+
+        public void CatSound()
+        {
+            Console.WriteLine("고양이 울음소리 냐용~");
+        }
+    }
+
+    public class Bird : Animal
+    {
+        public Bird() : base() { }
+
+        public void BirdSound()
+        {
+            Console.WriteLine("새 울음소리 짹짹~");
+        }
+    }
+}
---
+++ WindowsFormsApp_Casting_Test/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Casting_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Animal dog = new Dog();
+            dog.Name = "강아지";
+            dog.Speak();
+            ((Dog)dog).DogSound(); 
+
+            Animal cat = new Cat();
+            cat.Name = "고양이";
+            cat.Speak();
+            ((Cat)cat).CatSound();
+
+            Animal Bird = new Bird();
+            Bird.Name = "새";
+            Bird.Speak();
+            ((Bird)Bird).BirdSound();
+        }
+    }
+}
